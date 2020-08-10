4459fca0569a57f21e6c9fb9a5f8d0801174e2b0
FernandoDeSosa <fernandodesosa@gmail.com>
2020-08-10 09:31:02 -0300

    Punto 2

--- dos.js
+++ dos.js
@@ -7,5 +7,82 @@ b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  var respuesta = true;
+  var marca;
+  var precio;
+  var pesoKilogramos;
+  var tipo;
+  var pesoTotalCompra = 0;
+  var banderaLiquido = true;
+  var precioLiquidoMasCaro;
+  var marcaLiquidoMasCaro;
+  var pesoMasLivianoSolidos;
+  var marcaMasLivianoSolidos;
+  var banderaSolido = true;
+  
+
+
+  while(respuesta)
+  {
+    do{
+      marca = prompt("Ingrese marca");
+    }while(!(isNaN(marca)));
+
+    do{
+      precio = prompt("Ingrese precio");
+      precio = parseInt(precio);
+    }while(isNaN(precio));
+
+    do{
+      pesoKilogramos = prompt("Ingrese peso en kilogramos");
+      pesoKilogramos = parseInt(pesoKilogramos);
+    }while(isNaN(pesoKilogramos));
+
+    do{
+      tipo = prompt("Ingrese tipo solido o liquido");
+    }while(tipo != "solido" && tipo != "liquido");
+
+    //a)informar el peso total de la compra.
+
+    pesoTotalCompra+= pesoKilogramos;
+
+    //b)la marca del más caro de los líquidos
+
+    if(tipo == "liquido" && banderaLiquido){
+      precioLiquidoMasCaro = precio;
+      marcaLiquidoMasCaro = marca;
+      banderaLiquido = false;
+    }else if(precio > precioLiquidoMasCaro){
+      precioLiquidoMasCaro = precio;
+      marcaLiquidoMasCaro = marca;
+    }
+    if(tipo == "liquido"){
+      console.log("La marca del liquido mas caro " + marcaLiquidoMasCaro);
+    }
+
+    //c)la marca del más liviano de los sólidos
+
+    if(tipo == "solido" && banderaSolido){
+      pesoMasLivianoSolidos = pesoKilogramos;
+      marcaMasLivianoSolidos = marca;
+      banderaSolido = false;
+    }else if(pesoKilogramos < pesoMasLivianoSolidos){
+      pesoMasLivianoSolidos = pesoKilogramos;
+      marcaMasLivianoSolidos = marca;
+    }
+    if(tipo == "solido"){
+      console.log("La marca del solido mas liviano es " + marcaMasLivianoSolidos);
+    }
+
+
+
+
+    respuesta = confirm("Desea ingresar otro?");
+  }//fin de while
+
+  //a
+  console.log("El peso total de la compra es " + pesoTotalCompra);
+
+
+
 }

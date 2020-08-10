d8a59d8d56706a222be944b5be24df0d134a21c8
FernandoDeSosa <fernandodesosa@gmail.com>
2020-08-10 10:10:54 -0300

    Punto 3

--- tres.js
+++ tres.js
@@ -11,5 +11,98 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	alert("tres");
+	var respuesta = true;
+	var nombreTitular;
+	var lugar;
+	var temporada;
+	var cantidadPersonas;
+	var contadorBariloche = 0;
+	var contadorCataratas = 0;
+	var contadorSalta = 0;
+	var nombreTitularMayorPasajeros;
+	var cantidadPersonasMayor;
+	var bandera = true;
+	var contadorPersonas = 0;
+	var acumuladorPersonas = 0;
+	var promedio;
+
+	while(respuesta)
+	{
+		do{
+			nombreTitular = prompt("Ingrese nombre del titular");
+		}while(!(isNaN(nombreTitular)));
+
+		do{
+			lugar = prompt("Ingrese lugar");
+		}while(lugar != "bariloche" && lugar != "cataratas" && lugar != "salta");
+
+		do{
+			temporada = prompt("Ingrese temporada");
+		}while(temporada != "otoño" && temporada != "invierno" && temporada != "verano" && temporada != "primavera");
+
+		do{
+			cantidadPersonas = prompt("Ingrese cantidad de personas");
+			cantidadPersonas = parseInt(cantidadPersonas);
+		}while(isNaN(cantidadPersonas));
+
+		//a)el lugar más elegido
+
+		switch(lugar){
+			case "bariloche":
+				contadorBariloche++;
+				break;
+			case "cataratas":
+				contadorCataratas++;
+				break;
+			case "salta":
+				contadorSalta++;
+				break;
+		}
+
+		//b)el nombre de titular que lleva más pasajeros.
+
+		if(bandera){
+			cantidadPersonasMayor = cantidadPersonas;
+			nombreTitularMayorPasajeros = nombreTitular;
+			bandera = false;
+		}else if(cantidadPersonas > cantidadPersonasMayor){
+			cantidadPersonasMayor = cantidadPersonas;
+			nombreTitularMayorPasajeros = nombreTitular;
+		}
+
+		//c)el promedio de personas por viaje,  que viajan en invierno
+
+		if(temporada == "invierno"){
+			acumuladorPersonas+= cantidadPersonas;
+			contadorPersonas++;
+		}
+		promedio = acumuladorPersonas / contadorPersonas;
+	
+	
+		if(temporada == "invierno"){
+		console.log("El promedio de personas que viaja en invierno es " + promedio);
+		}
+		
+		
+
+
+
+
+		respuesta = confirm("Desea ingresar otro?");
+	}//fin de while
+
+	//a
+	if(contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta){
+		console.log("El lugar mas elegido es Bariloche");
+	}else if(contadorCataratas > contadorSalta){
+		console.log("El lugar mas elegido es Cataratas");
+	}else{
+		console.log("El lugar mas elegido es Salta");
+	}
+	//b
+	console.log("El nombre del titular con mayor pasajeros es " + nombreTitularMayorPasajeros);
+	
+
+
+
 }
